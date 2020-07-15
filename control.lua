f9e2d07250f0b002c3e774e42f3b41b72510ce6f
Dominik Galbavý <anders142536@gmail.com>
2020-07-15 19:05:00 +0200

    added stuff

--- control.lua
+++ control.lua
@@ -1,11 +1,13 @@
--- 7200 = 2 * 60 * 60 ticks, every 10 mins
+-- 7200 = 2 * 60 * 60 ticks, every 2 mins
 script.on_nth_tick(7200, function()
 	game.take_screenshot{
 		resolution={4000, 4000},
 		position={0, 0},
 		zoom=0.3,		-- lower means further zoomed out
 		daytime=0,		-- bright daylight
-		path="./screenshots/screenshot" .. game.tick .. ".png"
+		water_tick=0,
+		by_player=Anders142536,
+		path="./screenshots/" .. game.default_map_gen_settings.seed .. "/" .. "screenshot" .. game.tick .. ".png"
 	}
 	game.print("screenshot done");
 end)
