b3cfc763f2b45a9476ae4edce9a593226b0ead35
Dominik Galbavý <anders142536@gmail.com>
2020-07-15 20:52:25 +0200

    removed message printed on screenshot

--- control.lua
+++ control.lua
@@ -1,13 +1,12 @@
 -- 7200 = 2 * 60 * 60 ticks, every 2 mins
 script.on_nth_tick(7200, function()
 	game.take_screenshot{
-		resolution={4000, 4000},
+		resolution={7680, 4320},
 		position={0, 0},
 		zoom=0.3,		-- lower means further zoomed out
 		daytime=0,		-- bright daylight
 		water_tick=0,
-		by_player=Anders142536,
+		by_player="Anders142536",
 		path="./screenshots/" .. game.default_map_gen_settings.seed .. "/" .. "screenshot" .. game.tick .. ".png"
 	}
-	game.print("screenshot done");
 end)
