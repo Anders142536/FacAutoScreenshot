6e9e89613e7f192030ca2aa8d160dd25931fa71e
Anders <dom.galbavy@gmail.com>
2020-07-15 17:45:09 +0200

    first commit

---
+++ control.lua
@@ -0,0 +1,11 @@
+-- 7200 = 2 * 60 * 60 ticks, every 10 mins
+script.on_nth_tick(7200, function()
+	game.take_screenshot{
+		resolution={4000, 4000},
+		position={0, 0},
+		zoom=0.3,		-- lower means further zoomed out
+		daytime=0,		-- bright daylight
+		path="./screenshots/screenshot" .. game.tick .. ".png"
+	}
+	game.print("screenshot done");
+end)
