69ad0c5a51108802553898a3df00819800c15939
Anders <dom.galbavy@gmail.com>
2020-07-19 23:56:23 +0200

    added locale, screenshots now based on player settings

--- control.lua
+++ control.lua
@@ -1,16 +1,35 @@
-resX = 7680;
-resY = 4320;
-
 script.on_init(function()
 	game.print("FacAutoScreenshot enabled and initialized")
-	global.zoom = 1;
-	evaluateZoomFromWholeBase();
+	initialize();
 end)
 
 script.on_configuration_changed(function()
 	game.print("Something changed. Reevaluating FacAutoScreenshot")
-	global.zoom = 1;
+	initialize();
+end)
+
+function initialize()
+	if global.zoom == nil then
+		global.zoom = 1;
+	end
 	evaluateZoomFromWholeBase();
+end
+
+-- 3600
+script.on_nth_tick(60, function(event)
+	for _, player in pairs(game.connected_players) do
+		game.print("player!")
+		local index = player.index
+		local doScreenshot = settings.get_player_settings(game.get_player(index))["FAS-do-screenshot"].value
+		if doScreenshot then
+			local interval = settings.get_player_settings(game.get_player(index))["FAS-Screenshot-interval"].value * 60
+			
+			if event.tick % interval == 0 then
+				game.print("ding")
+				renderScreenshot(index)
+			end
+		end
+	end
 end)
 
 script.on_load(function()
@@ -23,8 +42,8 @@ function on_built_entity(event)
 	-- 7680					resx
 	-- -------- = 0,3		------------------ = zoom
 	-- 800  32				leftRight resTiles
-	zoomX = resX / (math.abs(pos.x) * 2 * 32);
-	zoomY = resY / (math.abs(pos.y) * 2 * 32);
+	zoomX = global.resX / (math.abs(pos.x) * 2 * 32);
+	zoomY = global.resY / (math.abs(pos.y) * 2 * 32);
 	
 	if breaksCurrentZoom(zoomX, zoomY) then
 		evaluateZoomFromPosition(zoomX, zoomY)
@@ -32,18 +51,33 @@ function on_built_entity(event)
 	end
 end
 
--- 7200 = 2 * 60 * 60 ticks, every 10 mins
-script.on_nth_tick(7200, function()
+function renderScreenshot(index)
+	local resolution = settings.get_player_settings(game.get_player(index))["FAS-Resolution"].value
+
+	local resX = 7680;
+	local resY = 4320;
+	if (resolution == "3840x2160 (4K)") then
+		resX = 3840;
+		resY = 2160;
+	elseif (resolution == "1920x1080 (FullHD)") then
+		resX = 1920;
+		resY = 1080;
+	elseif (resolution == "1280x720  (HD)") then
+		resX = 1280;
+		resY = 720;
+	end
+
+	game.print("doing screenshot with res " .. resX .. " and player " .. index)
 	game.take_screenshot{
 		resolution={resX, resY},
 		position={0, 0},
 		zoom=global.zoom,		-- lower means further zoomed out
 		daytime=0,		-- bright daylight
 		water_tick=0,
-		by_player="Anders142536",
+		by_player=index,
 		path="./screenshots/" .. game.default_map_gen_settings.seed .. "/" .. "screenshot" .. game.tick .. ".png"
 	}
-end)
+end
 
 function breaksCurrentZoom(zoomX, zoomY)
 	return (zoomX < global.zoom or zoomY < global.zoom);
