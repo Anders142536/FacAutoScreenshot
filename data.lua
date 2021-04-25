69137c248c01ab0670856d762d52b8f95a28405b
Anders <dom.galbavy@gmail.com>
2021-04-25 18:26:12 +0200

    added selection tool for area selection

--- data.lua
+++ data.lua
@@ -11,5 +11,20 @@ data:extend{
         type = "custom-input",
         name = "FAS-right-click",
         key_sequence = "mouse-button-2"
+    },
+    {
+        type = "selection-tool",
+        name = "FAS-selection-tool",
+        icon = "__FacAutoScreenshot__/graphics/FAS-24px.png",
+        icon_size = 24,
+        flags = {"hidden", "not-stackable", "spawnable"},
+        stack_size = 1,
+        selection_color = {0, 0, 0, 0},
+        alt_selection_color = {0, 0, 0, 0},
+        selection_mode = {"nothing"},
+        alt_selection_mode = {"nothing"},
+        selection_cursor_box_type = "entity",
+        alt_selection_cursor_box_type = "entity",
+        mouse_cursor = "selection-tool-cursor"
     }
 }
