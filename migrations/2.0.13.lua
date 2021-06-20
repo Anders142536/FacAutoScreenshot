2ef7c33d1de3a78483281a3cbf261cf3a5fa7ea2
Anders <dom.galbavy@gmail.com>
2021-06-20 16:08:17 +0200

    fixed migration issue

--- migrations/2.0.13.lua
+++ migrations/2.0.13.lua
@@ -1,7 +1,8 @@
 local correctResXList = {15360, 7680, 3840, 1920, 1280}
-
-for index, player in pairs(global.auto) do
-    if correctResXList[player.resolution_index] ~= player.resX then
-        global.auto[index].resolution_index = player.resolution_index + 1
+if global.auto then
+    for index, player in pairs(global.auto) do
+        if correctResXList[player.resolution_index] ~= player.resX then
+            global.auto[index].resolution_index = player.resolution_index + 1
+        end
     end
 end
